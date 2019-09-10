2d8fbbfa6c73718591fd9de0884d313eff99342b
Jeffrey Yau <jeffreyyau@outlook.com>
2019-09-10 16:40:23 +0800

    feat: Update contact section and ditch footer

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 // AOS.init();
 
-let navbar = document.getElementById('navbar');
-let about = document.getElementById('about');
+// let navbar = document.getElementById('navbar');
+// let about = document.getElementById('about');
 
-window.onscroll = function() {
-    let position = navbar.getBoundingClientRect();
-    let position2 = about.getBoundingClientRect();
-    // this.console.log(position);
-    if (position.top >= position2.top) {
-        navbar.classList.remove('navbar-dark');
-    }
-    else {
-        navbar.classList.add('navbar-dark');
-    }
-}
+// window.onscroll = function() {
+//     let position = navbar.getBoundingClientRect();
+//     let position2 = about.getBoundingClientRect();
+//     // this.console.log(position);
+//     if (position.top >= position2.top) {
+//         navbar.classList.remove('navbar-dark');
+//     }
+//     else {
+//         navbar.classList.add('navbar-dark');
+//     }
+// }
